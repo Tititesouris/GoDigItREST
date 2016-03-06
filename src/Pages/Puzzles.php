e186943040d39a4bc1826851651641dcefc19924
Yamelio <guillaume.kleinpoort@gmail.com>
2016-03-06 03:17:09 +0100

    Screw qrcodes

--- src/Pages/Puzzles.php
+++ src/Pages/Puzzles.php
@@ -14,7 +14,7 @@ class Puzzles extends Page
 
     public function POST($parameters)
     {
-        if (!empty($parameters) && isset($parameters["name"], $parameters["description"], $parameters["qrcode"], $parameters["latitude"], $parameters["longitude"])) {
+        if (!empty($parameters) && isset($parameters["name"], $parameters["description"],/* $parameters["qrcode"],*/ $parameters["latitude"], $parameters["longitude"])) {
             return PuzzlesTable::addPuzzle(new PuzzleModel(6, $parameters["name"], $parameters["description"], $parameters["qrcode"], $parameters["latitude"], $parameters["longitude"]));
         }
         return array("error" => "Invalid parameters");
@@ -22,7 +22,10 @@ class Puzzles extends Page
 
     public function PUT($parameters)
     {
-        return array("error" => "Not Implemented yet");
+        if (!empty($parameters) && isset($parameters["id"], $parameters["name"], $parameters["description"], $parameters["qrcode"], $parameters["latitude"], $parameters["longitude"])) {
+            return false;
+        }
+        return array("error" => "Invalid parameters");
     }
 
     public function DELETE($parameters)
